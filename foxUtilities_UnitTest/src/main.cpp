b8db69abae2db39d7a43c98cb035d2bd9a5e9013
Jorge Alexandro Zamudio Arredondo <idv15c.jzamudio@uartesdigitales.edu.mx>
2018-09-28 08:13:45 -0500

    Set correctly the file for Unit Tests

--- foxUtilities_UnitTest/src/main.cpp
+++ foxUtilities_UnitTest/src/main.cpp
@@ -1,7 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <foxPlatformMath.h>
+
+using namespace foxEngineSDK;
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+
+TEST(Utilities, Math)
+{
+
 }
