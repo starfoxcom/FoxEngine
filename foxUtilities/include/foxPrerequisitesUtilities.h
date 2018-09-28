ed1e851633472e6c25addd72339597e2b93d8441
Jorge Alexandro Zamudio Arredondo <idv15c.jzamudio@uartesdigitales.edu.mx>
2018-09-28 08:11:02 -0500

    Add foxStdHeaders.h and foxPrerequisitesUtilities.h to project

---
+++ foxUtilities/include/foxPrerequisitesUtilities.h
@@ -0,0 +1,19 @@
+/**
+* @file foxPrerequisitesUtilities.h
+* @author ---
+* @date ---
+* @brief Utility include with the most basic defines needed
+*
+* Utility include with the most basic defines needed for compilation
+* This is the file that must be included for use on external objects
+*/
+
+#pragma once
+
+#include <cassert>
+
+#include "foxPlatformDefines.h"
+#include "foxPlatformTypes.h"
+#include "foxStdHeaders.h"
+//TODO: fwd declaration utilities
+
---
+++ foxUtilities/include/foxStdHeaders.h
@@ -0,0 +1,62 @@
+/**
+* @file foxStdHeaders.h
+* @author ---
+* @date ---
+* @brief Header for the STD libraries
+*
+* This file includes the most common and required STL objects
+* It takes account of the operating system of the build to modify
+* the required object
+*/
+
+#pragma once
+
+/**
+ * C type objects
+ */
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cstdarg>
+#include <cmath>
+
+/**
+* For memory management
+*/
+#include <memory>
+
+/**
+* STL containers
+*/
+#include <vector>
+#include <stack>
+#include <map>
+#include <string>
+#include <set>
+#include <list>
+#include <deque>
+#include <queue>
+#include <bitset>
+#include <array>
+#include <forward_list>
+
+#include <unordered_map>
+#include <unordered_set>
+
+/**
+* STL algorithms & functions
+*/
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <iterator>
+
+/**
+* Stream stuff
+*/
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
